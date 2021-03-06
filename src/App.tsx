0b91e58c6a8e8c4dc29ad599494a8545ccf4f871
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2021-03-06 14:29:59 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,19 @@ import {
   Code,
   Grid,
   theme,
+  InputRightElement,
+  Stack,
 } from "@chakra-ui/react"
+import Form from './Form';
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
+      <Stack justifySelf="flex-end">
+        <ColorModeSwitcher />
+      </Stack>
+      <Box width="100%" height="100vh" display="flex" alignItems="center" justifyContent="center" flexDirection="column" >
+       <Form />
+      </Box>
+    </ChakraProvider>
 )
